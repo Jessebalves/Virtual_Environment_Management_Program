2f26e910a66635bb0f2752762fd0db5c1149e6f4
unknown <jessebalves@gmail.com>
2025-04-28 18:41:24 -0400

    Implemented Graphical User Interface (GUI). Implemented exit prgram function

--- praco_copy.py
+++ praco_copy.py
@@ -1,7 +1,21 @@
+import tkinter
 import sys
 import os
 import shutil
 import time
+
+#This is an example of tkinter module
+#create Graphical User interface
+root = tkinter.Tk()
+
+#title for graphical user interfave
+root.title("VEM - Virtual Environment Management Program")
+
+#size of GUI
+root.geometry("1920x1080")
+
+text_widget = tkinter.Text(root,height =1920, width =1080)
+text_widget.pack()
 
 
 files_local_directory = []
@@ -14,7 +28,13 @@ thanks = "Thank you for checking out my program!"
 #subdirectory files are not being deleted
 #if theres a folder within a folder, the folder is appended to the wrong list        
 
-print("Operating System on machine: ", os.name)
+OP_Message = ('Operating System on machine: ' + os.name)
+print(OP_Message)
+
+text_widget.insert(tkinter.END,OP_Message)
+root.mainloop()
+
+#print("Operating System on machine: ", os.name)
 print("Current working directory: ",os.getcwd(),"\n")
 
 def scan_for_files_and_folders():
@@ -44,6 +64,14 @@ def delete_self():
     for file in files_local_directory:
         if file == "praco.py":
             os.remove(file)
+
+#function to exit program
+def exit_program():
+    for value in thanks:
+        print(value,end="")
+        time.sleep(0.003)
+    sys.exit(0)
+    
                         
 #Function that combines all the other functions together
 def traverse():
@@ -56,12 +84,14 @@ traverse()
 
 #sub directory for loops
 for value in folders_local_directory:
+    previous_dir = os.getcwd()
     os.chdir(value)
     for items in os.listdir():
         if os.path.isfile(items):
             files_sub_directory.append(items)
         if os.path.isdir(items):
             folders_sub_directory.append(items)
+    os.chdir(previous_dir)
 
 
 #Print statements
@@ -78,14 +108,14 @@ if user_message1 == "yes":
     user_message1 = input("What would you like to change the directory to?")
     print(os.getcwd())
 elif user_message1 == "no":
-    user_message2 = input("Would you like to exit the program?")
-    if user_message2 == "yes":
-        for value in thanks:
-            print(value,end="")
-            time.sleep(0.003)
-        sys.exit(0)
-    else:
-        print("You gotta add code here bro... this a deadend")
+    pass
+user_message2 = input("Would you like to exit the program?")
+if user_message2 == "yes":
+        exit_program()
+elif user_message2 == "no":
+    print("continue from here")
 else:
     print("sorry we dont quite understand")
     sys.exit(1)
+
+
